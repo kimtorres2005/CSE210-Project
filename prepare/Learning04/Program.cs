5bc5319fd60b82e666ff173225fb424f9d10bdbe
kimtorres2005 <kimtorres2005@gmail.com>
2023-05-30 21:39:03 -0500

    Learning activity four

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("Daniel Jenkins", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assign2 = new MathAssignment("Caleb Coates", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign2.GetHomeworkList());
+
+        WritingAssignment assign3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign3.GetWritingInfo());
     }
 }
