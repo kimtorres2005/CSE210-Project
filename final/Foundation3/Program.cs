0769305d30585c67a512610b061cacc91eee66ae
kimtorres2005 <kimtorres2005@gmail.com>
2023-07-08 15:04:03 -0500

    updates to program 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -6,7 +6,10 @@ class Program3
     {
         // Lecture event
         Console.WriteLine("Event 1 - Lecture\n");
-        Lecture lectureEvent = new Lecture
+        Lecture lectureEvent = new Lecture("Lecture", "Professor Thomas",
+        "Lecture about programming with classes with Michael Thomas in the STC in room 243!",
+        "8/12/2023", "5:00 PM", new Address("210 West 4th South", "Rexburg", "ID", "United States"),
+        "Michael Thomas", 100);
 
         // Getting each description
         Console.WriteLine(lectureEvent.getShortDescription());
@@ -15,9 +18,9 @@ class Program3
 
         // Reception event
         Console.WriteLine("\nEvent 2 - Reception\n");
-        Reception receptionEvent = new Reception("Reception", "Brenner and Emily's Wedding Reception",
-        "Please come to Brenner and Emily's wedding reception to celebrate their marriage!", "8/25/2023",
-        "3:00 PM", new Address("123 Idaho Falls Rd", "Idaho Falls", "ID", "United States"), "brenner98@gmail.com");
+        Reception receptionEvent = new Reception("Reception", "Brandon and Ashley's Wedding Reception",
+        "Please come to Brandon and Ashley's wedding reception to celebrate their marriage!", "9/23/2023",
+        "4:00 PM", new Address("321 Idaho Falls Rd", "Idaho Falls", "ID", "United States"), "brandon97@gmail.com");
 
         // Getting each description
         Console.WriteLine(receptionEvent.getShortDescription());
@@ -27,7 +30,7 @@ class Program3
         // Outdoor gathering event
         Console.WriteLine("\nEvent 3 - Outdoor Gathering\n");
         OutdoorGathering outdoorEvent = new OutdoorGathering("Outdoor Gathering", "Snowman Making",
-        "Come to the BYU-I stadium field to help each other make snowmen!", "2/26/2023", "8:00 PM",
+        "Come to the BYU-I stadium field to help each other make snowmen!", "1/26/2024", "8:00 PM",
         new Address("239 S 1st W", "Rexburg", "ID", "United States"), "Snowing, 5 inches of snow expected");
 
         // Getting each description
